bde6de13992efa0475e1c95359a885c240fd6e7e
mcorbin <corbin.math@gmail.com>
2021-08-06 23:08:11 +0200

    next snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fr.mcorbin/corbihttp "0.16.0"
+(defproject fr.mcorbin/corbihttp "0.17.0-SNAPSHOT"
   :description "Shared namespaces for my http projects"
   :url "https://github.com/mcorbin/corbihttp"
   :license {:name "EPL-2.0"
