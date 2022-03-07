c6dda8425eb53a192617eab7ef44563dfc76ba1e
mcorbin <corbin.math@gmail.com>
2022-03-08 00:11:46 +0100

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fr.mcorbin/corbihttp "0.28.0"
+(defproject fr.mcorbin/corbihttp "0.29.0"
   :description "Shared namespaces for my http projects"
   :url "https://github.com/mcorbin/corbihttp"
   :license {:name "EPL-2.0"
